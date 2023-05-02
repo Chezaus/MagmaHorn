c42e2cef7f38def8865c7d39970515b013b14336
Chezaus <tristanchen1000@duck.com>
2023-05-02 17:13:20 -0400

    Basic Movement
    -Added movement
    -Working on dashing

---
+++ Assets/Scripts/PlayerMovement.cs
@@ -0,0 +1,51 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerMovement : MonoBehaviour {
+
+	public CharacterController2D controller;
+	public Animator animator;
+
+	public float runSpeed = 40f;
+
+	float horizontalMove = 0f;
+	bool jump = false;
+
+	// Update is called once per frame
+	void Update () {
+
+		horizontalMove = Input.GetAxisRaw("Horizontal") * runSpeed;
+
+		animator.SetBool("isWalking", Mathf.Abs(horizontalMove) > 0);
+
+		if (Input.GetButtonDown("Jump"))
+		{
+			jump = true;
+			animator.SetBool("isJumping", true);
+		}	
+	}
+
+	public void OnLanding ()
+	{
+		animator.SetBool("isJumping", false);
+	}
+
+	void FixedUpdate ()
+	{
+		// Move our character
+		controller.Move(horizontalMove * Time.fixedDeltaTime, jump);
+		jump = false;
+	}
+
+
+
+
+
+
+
+
+
+	
+}
+
