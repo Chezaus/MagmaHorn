8c7b208514e0c7bd841a43d141192077b046484a
Chezaus <tristanchen1000@duck.com>
2023-06-01 16:58:52 -0400

    Health Bar
    -added health bar

---
+++ Assets/Scripts/HealthUI.cs
@@ -0,0 +1,33 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HealthUI : MonoBehaviour
+{
+    public Player main;
+    public CanvasGroup health1;
+    public CanvasGroup health2;
+    public CanvasGroup health3;
+    public CanvasGroup health4;
+    public CanvasGroup health5;
+    public CanvasGroup health6;
+
+    void Update()
+    {
+        switch(main.health){
+            case 6:  
+                break;
+            case 5: health1.alpha = 0;
+                break;
+            case 4:  health2.alpha = 0;
+                break;
+            case 3:  health3.alpha = 0;
+                break;
+            case 2:  health4.alpha = 0;
+                break;
+            case 1:  health5.alpha = 0;
+                break;
+        }
+    }
+
+}
