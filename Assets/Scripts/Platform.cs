8e8601051f794617eaf7f35d9c21bd2607a58ec3
Chezaus <tristanchen1000@duck.com>
2023-07-20 14:05:14 -0400

    Alpha 0.5
    -Added Platforms
    -Added new block
    -Minor bug fixes

---
+++ Assets/Scripts/Platform.cs
@@ -0,0 +1,32 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Platform : MonoBehaviour
+{
+        public GameObject player;
+        void OnTriggerEnter2D (Collider2D other) 
+        {
+        //make the parent platform ignore the jumper
+        var platform = transform.parent;
+        Physics2D.IgnoreCollision(other.GetComponent<PolygonCollider2D>(), platform.GetComponent<Collider2D>());
+    }
+     
+        void OnTriggerExit2D (Collider2D jumper) 
+        {
+        //re-enable collision between jumper and parent platform, so we can stand on top again
+        var platform = transform.parent;
+        Physics2D.IgnoreCollision(jumper.GetComponent<PolygonCollider2D>(), platform.GetComponent<Collider2D>(), false);
+    }
+
+        void Update()
+        {
+            if(Input.GetAxisRaw("Vertical") < 0)
+            {
+                var platform = transform.parent;
+                Physics2D.IgnoreCollision(player.GetComponent<PolygonCollider2D>(), platform.GetComponent<Collider2D>());
+            }
+
+        }
+
+}
